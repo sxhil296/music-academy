3560992ace23c3bb61fb0dca32f20e7973a36944
Sahil <leviack629@gmail.com>
2024-07-04 11:25:09 +0530

    layout and ui setup

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Contact = (props: Props) => {
+  return (
+    <div className='text-2xl text-center'>Contact</div>
+  )
+}
+
+export default Contact
