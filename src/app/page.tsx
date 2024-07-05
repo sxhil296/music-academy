2565ccee7134daef6e19a3e1f23eea6de2c4fce9
Sahil <leviack629@gmail.com>
2024-07-05 15:33:42 +0530

    other sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MeetOurInstructors from "@/components/MeetOurInstructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
+      <UpcomingWebinars />
+      <MeetOurInstructors />
     </main>
   );
 }
